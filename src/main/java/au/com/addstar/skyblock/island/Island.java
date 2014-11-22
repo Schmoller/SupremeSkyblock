4e1dec9b18aa5f3847a4943f1d3ad217be02bc96
Schmoller <steven.schmoll@gmail.com>
2014-11-22 14:48:44 +1100

    Fixed islands not having a creation time set

--- src/main/java/au/com/addstar/skyblock/island/Island.java
+++ src/main/java/au/com/addstar/skyblock/island/Island.java
@@ -63,6 +63,8 @@ public class Island
 			mMembers.put(id, null);
 		
 		mChallenges = new ChallengeStorage(this);
+		
+		mIslandStartTime = mLastUseTime = System.currentTimeMillis();
 	}
 	
 	public UUID getOwner()
